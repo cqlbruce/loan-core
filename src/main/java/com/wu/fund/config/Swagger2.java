0a1e78f71399bbe651f58ce61053b9080b5f26fb
wangying9 <wangying9@worldunion.com.cn>
2018-05-08 11:57:24 +0800

    初始化项目

---
+++ src/main/java/com/wu/fund/config/Swagger2.java
@@ -0,0 +1,43 @@
+package com.wu.fund.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+/**
+ * @Author: wangying
+ * @Description:
+ * @Date: Created in 17:29 2018/4/10
+ */
+@Configuration
+@EnableSwagger2
+public class Swagger2 {
+    //swagger2的配置文件，这里可以配置swagger2的一些基本的内容，比如扫描的包等等
+    @Bean
+    public Docket createRestApi() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(apiInfo())
+                .select()
+                //为当前包路径
+                .apis(RequestHandlerSelectors.basePackage("com.wu.fund.controller"))
+                .paths(PathSelectors.any())
+                .build();
+    }
+    //构建 api文档的详细信息函数,注意这里的注解引用的是哪个
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                //页面标题
+                .title("资金核心服务接口API")
+                //版本号
+                .version("1.0")
+                .build();
+    }
+
+
+}
---
+++ src/main/java/com/wu/fund/controller/FundController.java
@@ -0,0 +1,4 @@
+package com.wu.fund.controller;
+
+public class FundController {
+}

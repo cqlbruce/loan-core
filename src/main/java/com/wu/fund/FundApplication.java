bb07dcf2986e4fad63958a49c0386b45d7435a2d
wangying9 <wangying9@worldunion.com.cn>
2018-05-10 18:52:57 +0800

    eraka配置

--- src/main/java/com/wu/fund/FundApplication.java
+++ src/main/java/com/wu/fund/FundApplication.java
@@ -3,11 +3,13 @@ package com.wu.fund;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.feign.EnableFeignClients;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @MapperScan("com.wu.fund.dao")
 @ComponentScan("com.wu.fund")
+@EnableFeignClients
 public class FundApplication  {
 
     public static void main(String[] args) {

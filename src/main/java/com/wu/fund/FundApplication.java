e663a1c5cfdf59f0e334452bad97559892c3d5e2
wangying9 <wangying9@worldunion.com.cn>
2018-05-11 11:00:38 +0800

    eraka配置

--- src/main/java/com/wu/fund/FundApplication.java
+++ src/main/java/com/wu/fund/FundApplication.java
@@ -3,16 +3,31 @@ package com.wu.fund;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
+
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @MapperScan("com.wu.fund.dao")
 @ComponentScan("com.wu.fund")
 @EnableFeignClients
+//@EnableHystrixDashboard
+//@EnableTurbine
+//@EnableCircuitBreaker
 public class FundApplication  {
 
     public static void main(String[] args) {
         SpringApplication.run(FundApplication.class, args);
     }
+    @Bean
+    @LoadBalanced
+    public RestTemplate restTemplate() {
+        //用于调用"提供者"的方法
+        return new RestTemplate();
+    }
+
 }

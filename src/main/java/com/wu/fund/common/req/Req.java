6f3921c7784c2649ca263c8683ae2a23f94ba29a
wangying9 <wangying9@worldunion.com.cn>
2018-05-14 11:07:29 +0800

    申请授信或预审批查询

--- src/main/java/com/wu/fund/common/req/Req.java
+++ src/main/java/com/wu/fund/common/req/Req.java
@@ -1,5 +1,7 @@
 package com.wu.fund.common.req;
 
+import io.swagger.annotations.ApiModelProperty;
+
 import java.io.Serializable;
 
 /**
@@ -12,11 +14,13 @@ public class Req<T> implements Serializable {
     /**
      * 请求头
      */
+    @ApiModelProperty(value = "请求头", required = true)
     private ReqHeader reqHeader;
 
     /**
      * 请求内容
      */
+    @ApiModelProperty(value = "请求内容", required = true)
     private T reqBody;
 
 
--- src/main/java/com/wu/fund/common/req/ReqHeader.java
+++ src/main/java/com/wu/fund/common/req/ReqHeader.java
@@ -1,5 +1,7 @@
 package com.wu.fund.common.req;
 
+import io.swagger.annotations.ApiModelProperty;
+
 /**
  * @Author: wangying
  * @Description:接受的请求头
@@ -9,16 +11,19 @@ public class ReqHeader {
     /**
      * 通道
      */
+    @ApiModelProperty(value = "通道")
     private String channelNo;
 
     /**
-     * 授信或预审批请求流水号
+     * 请求流水号
      */
+    @ApiModelProperty(value = "请求流水号", required = true)
     private String applyNo;
 
     /**
      * 业务流水号
      */
+    @ApiModelProperty(value = "业务流水号")
     private String busiNo;
 
 
